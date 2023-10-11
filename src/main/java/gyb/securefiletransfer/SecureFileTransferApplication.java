ddc598d143271cdf542d2e681b022bdd8aee7091
gaoyubo2 <673354892@qq.com>
2023-10-11 17:04:04 +0800

    初始

---
+++ src/main/java/gyb/securefiletransfer/SecureFileTransferApplication.java
@@ -0,0 +1,13 @@
+package gyb.securefiletransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFileTransferApplication.class, args);
+	}
+
+}
